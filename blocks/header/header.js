d1b9c8fae2a5c895cbbfd842f205bcd2a95c4f5b
Hannes Hertach <hanneshertach@gmail.com>
2023-09-28 10:41:24 +0200

    change header link

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -155,7 +155,7 @@ export default async function decorate(block) {
       <div class="nav-close-background"></div>
       <div class="nav-hamburger"><span class="burger"></span></div>
       <div class='nav-logo'>
-        <a href="/${getLanguage()}/" aria-label="Link to home page">
+        <a href="https://www.hkbu.edu.hk/" aria-label="Link to home page">
           <span class="icon icon-logo-white"></span>
           <span class="icon icon-logo-small"></span>
         </a>
